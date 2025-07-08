144c5fb8b0a7033a80175596faf08a2433f7ddb1
Anutithi4309 <elorasumi1977@gmail.com>
2025-07-08 22:05:22 +0600

    Add files via upload

---
+++ filessss.py
@@ -0,0 +1,9 @@
+file1=open('Codingal.txt','r')
+file2=open('Codingalupdated.txt','w')
+for line in file1.readlines():
+    if not (line.startswith('coding')):
+        print(line)
+        file2.write(line)
+
+file1.close()
+file2.close()
